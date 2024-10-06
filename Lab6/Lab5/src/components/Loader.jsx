f351d5cd4bb6a66fe8d09c9fd177bd3f2b0fd7a0
StasKoven <150803645+StasKoven@users.noreply.github.com>
2024-10-06 23:33:48 +0300

    Update lab5

--- Lab6/Lab5/src/components/Loader.jsx
+++ Lab6/Lab5/src/components/Loader.jsx
@@ -3,14 +3,13 @@ import React from 'react';
 const Loader = ({ isLoading, children }) => {
   return (
     <>
-      {isLoading ? (
+      {isLoading && (
         <div style={{ textAlign: 'center', marginTop: '50px' }}>
           <div className="spinner"></div>
           <p>Loading...</p>
         </div>
-      ) : (
-        children
       )}
+      {children}
     </>
   );
 };
