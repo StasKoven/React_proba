57e41eb0c44baffc7df6ac5164f6f3ac9005723f
StasKoven <150803645+StasKoven@users.noreply.github.com>
2024-10-08 11:50:28 +0300

    Search

--- Lab8/src/App.jsx
+++ Lab8/src/App.jsx
@@ -5,6 +5,7 @@ import AddressTable from './components/AddressTable';
 const App = () => {
   const [contacts, setContacts] = useState([]);
   const [editContact, setEditContact] = useState(null);
+  const [searchTerm, setSearchTerm] = useState('');
 
   const addContact = (newContact) => {
     setContacts([...contacts, { id: contacts.length + 1, ...newContact }]);
@@ -19,9 +20,24 @@ const App = () => {
     setEditContact(null);
   };
 
+  const filteredContacts = contacts.filter(
+    (contact) =>
+      contact.firstName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      contact.lastName.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
+
     <div className="container">
       <h1>Address Book</h1>
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Search by name..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)} 
+        />
+      </div>
       <AddressForm addContact={addContact} editContact={editContact} updateContact={updateContact} />
       <AddressTable contacts={contacts} setEditContact={setEditContact} />
     </div>
