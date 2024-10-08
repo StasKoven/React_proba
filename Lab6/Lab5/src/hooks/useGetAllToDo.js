72ed4fef03063653e5ee72b796460f4df2527e40
StasKoven <150803645+StasKoven@users.noreply.github.com>
2024-10-08 13:33:32 +0300

    Update lab5

--- Lab6/Lab5/src/hooks/useGetAllToDo.js
+++ Lab6/Lab5/src/hooks/useGetAllToDo.js
@@ -1,31 +1,31 @@
 import { useState, useEffect } from "react";
 
 const useGetAllToDo = () => {
-    const [isLoading, setIsLoading] = useState(false);
-    const [data, setData] = useState(null);
-    const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        setIsLoading(true);
-
-        const fetchData = async () => {
-        try {
-            fetch("https://jsonplaceholder.typicode.com/todos")
-            .then(response => response.json())
-            .then(json => {
-            setData(json);
-            setIsLoading(false);
-            });
-        } catch (error) {
-            setError(error);
-        } finally {
-            setIsLoading(false);
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
         }
+        const json = await response.json();
+        setData(json);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
     };
-    fetchData();
-    }, []);
 
-    return { isLoading, data, error };
+    fetchData();
+  }, []);
+
+  return { isLoading, data, error };
 };
 
 export default useGetAllToDo;
